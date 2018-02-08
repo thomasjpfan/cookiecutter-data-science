e2432ce8d7d73329f40d05b25ffc6177f27bbef2
Thomas Fan <thomasjpfan@gmail.com>
2018-02-08 13:36:46 -0500

    ENH: Add default dirs

--- {{cookiecutter.repo_name}}/src/files.py
+++ {{cookiecutter.repo_name}}/src/files.py
@@ -5,10 +5,12 @@ import os
 class RawFiles:
 
     def __init__(self, data_dir):
+        raw_dir = os.path.join(data_dir, "raw")
         pass
 
 
 class ProcessFiles:
 
     def __init__(self, data_dir):
+        proc_dir = os.path.join(data_dir, "processed")
         pass
