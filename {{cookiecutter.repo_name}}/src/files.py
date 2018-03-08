77937c1a9bf327e1da1efcd44a6a0f48005225a9
Thomas Fan <thomasjpfan@gmail.com>
2018-03-08 12:27:54 -0500

    ENH: Include root dir into files obj

--- {{cookiecutter.repo_name}}/src/files.py
+++ {{cookiecutter.repo_name}}/src/files.py
@@ -5,12 +5,10 @@ import os
 class RawFiles:
 
     def __init__(self, data_dir):
-        raw_dir = os.path.join(data_dir, "raw")
-        # add raw file names
+        self.raw_dir = os.path.join(data_dir, "raw")
 
 
 class ProcessFiles:
 
     def __init__(self, data_dir):
-        proc_dir = os.path.join(data_dir, "processed")
-        # add processed files names
+        self.proc_dir = os.path.join(data_dir, "processed")
