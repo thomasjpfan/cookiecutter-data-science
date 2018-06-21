a4422ea9d7df12de5de935a2714b8100a88470b7
Thomas Fan <thomasjpfan@gmail.com>
2018-06-20 20:29:48 -0400

    ENH: Adds skorch sample

---
+++ {{cookiecutter.repo_name}}/exp/simple_nn_model.py
@@ -0,0 +1,90 @@
+"""Linear Model"""
+import os
+
+from sacred import Experiment
+import numpy as np
+from sklearn.datasets import make_classification
+from torch import nn
+import torch.nn.functional as F
+
+from skorch import NeuralNetClassifier
+from skorch.callbacks import EpochScoring
+
+from exp.utils import add_common_config, get_params
+
+exp = Experiment("simple_nn_model")
+exp.add_config(tags=["simple_nn_model"])
+add_common_config(exp, record_local=True)
+params = get_params()
+
+
+class MyModule(nn.Module):
+    def __init__(self, num_units=10, nonlin=F.relu):
+        super(MyModule, self).__init__()
+
+        self.dense0 = nn.Linear(20, num_units)
+        self.nonlin = nonlin
+        self.dropout = nn.Dropout(0.5)
+        self.dense1 = nn.Linear(num_units, 10)
+        self.output = nn.Linear(10, 2)
+
+    def forward(self, X, **kwargs):
+        X = self.nonlin(self.dense0(X))
+        X = self.dropout(X)
+        X = F.relu(self.dense1(X))
+        X = F.softmax(self.output(X), dim=-1)
+        return X
+
+
+net = NeuralNetClassifier(
+    MyModule,
+    max_epochs=1,
+    lr=0.1,
+    callbacks=[
+        EpochScoring(
+            'accuracy', name='train_acc', lower_is_better=False, on_train=True)
+    ])
+
+
+@exp.command
+def predict(model_id, run_dir, _log):
+    model_fn = os.path.join(run_dir, params.simple_nn__model_fn)
+    predict_fn = os.path.join(run_dir, params.simple_nn__prediction_fn)
+
+    X, _ = make_classification(1000, 20, n_informative=10, random_state=0)
+    X = X.astype(np.float32)
+    net.initialize()
+    net.load_params(model_fn)
+
+    y_predict = net.predict_proba(X)
+    np.save(predict_fn, y_predict)
+
+    _log.info(f"Finished prediction, model_id: {model_id}")
+
+
+@exp.command
+def train_hp(model_id, _log, _run):
+    _log.info(f"No hyper parameter training")
+
+
+@exp.command
+def train(model_id, run_dir, _log, _run):
+    model_fn = os.path.join(run_dir, params.simple_nn__model_fn)
+    history_fn = os.path.join(run_dir, params.simple_nn__history_fn)
+
+    X, y = make_classification(1000, 20, n_informative=10, random_state=0)
+    X = X.astype(np.float32)
+    y = y.astype(np.int64)
+    net.fit(X, y)
+
+    net.save_params(model_fn)
+    net.save_history(history_fn)
+
+    valid_score = net.history[-1, 'valid_acc']
+    train_score = net.history[-1, 'train_acc']
+
+    _log.warning(f"Finished training, model_id: {model_id}, val_score: "
+                 f"{valid_score:0.6}, "
+                 f"train_score: {train_score:0.6}")
+    # valid score/error, train score/error
+    return [valid_score, train_score]
