8407637d08a92c9ba64b595fa5f1018233027eb5
Thomas Fan <thomasjpfan@gmail.com>
2018-07-22 17:06:42 -0400

    REV: Check anyways

--- {{cookiecutter.repo_name}}/download.sh
+++ {{cookiecutter.repo_name}}/download.sh
@@ -5,6 +5,10 @@ NAME=""
 
 dl_kaggle() {
 	local filename="$1"
+	if [ -f "$filename" ]; then
+		echo "$filename already downloaded"
+		return
+	fi
 	kaggle competitions download -c $NAME -f "$filename" -w
 }
 
