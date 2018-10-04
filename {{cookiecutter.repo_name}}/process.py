5bfefa13eeb19ead6d561482744aeb2143d4d447
Thomas Fan <thomasjpfan@gmail.com>
2018-10-04 13:49:53 -0400

    RFC: Adds cache decorators

--- {{cookiecutter.repo_name}}/process.py
+++ {{cookiecutter.repo_name}}/process.py
@@ -4,27 +4,23 @@ Create processed files
 import argparse
 
 import pandas as pd
-from mltome.cache import from_dataframe_cache
 
-from utils import get_params
+from utils import get_params, from_dataframe_cache
 
 
-@from_dataframe_cache('files__proc_train')
+@from_dataframe_cache("files__proc_train")
 def get_train(params, force=False, **kwargs):
     tr = pd.read_csv(params.files__raw_train)
     return tr
 
 
-@from_dataframe_cache('files__proc_test')
+@from_dataframe_cache("files__proc_test")
 def get_test(params, force=False, **kwargs):
     te = pd.read_csv(params.files__raw_test)
     return te
 
 
-PROCESS_FUNCS = {
-    "files__proc_train": get_train,
-    "files__proc_test": get_test,
-}
+PROCESS_FUNCS = {"files__proc_train": get_train, "files__proc_test": get_test}
 
 PROCESS_CHOICES = [
     key.split("files__proc_", maxsplit=1)[1] for key in PROCESS_FUNCS
@@ -37,28 +33,30 @@ def process(args):
 
     params = get_params()
 
-    key = f'files__proc_{key}'
+    key = f"files__proc_{key}"
     try:
         process_fn = params[key]
     except KeyError:
-        print(f'files/processed/{key} does not exists in params')
+        print(f"files/processed/{key} does not exists in params")
         return
 
     if process_fn.exists() and not force:
-        print(f'{process_fn} already exists, use --force')
+        print(f"{process_fn} already exists, use --force")
         return
 
-    print(f'Processing {key} filename: {process_fn}')
+    print(f"Processing {key} filename: {process_fn}")
     PROCESS_FUNCS[key](params, force)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        'key', choices=PROCESS_CHOICES, help='key of processed file')
+        "key", choices=PROCESS_CHOICES, help="key of processed file"
+    )
     parser.add_argument(
-        '-f', '--force', action='store_true', help='rerun processing for key')
+        "-f", "--force", action="store_true", help="rerun processing for key"
+    )
     parser.set_defaults(func=process)
 
     args = parser.parse_args()
--- {{cookiecutter.repo_name}}/utils.py
+++ {{cookiecutter.repo_name}}/utils.py
@@ -1,18 +1,62 @@
 import os
+from functools import wraps
 import datetime
 from pathlib import Path
 import argparse
 import logging
+import pandas as pd
 from pprint import pformat
 
 import yaml
 from munch import munchify
 
 
+def from_dataframe_cache(key):
+    """Returns a decorator that wraps a function with signature,
+    (params: dict, force: bool, kwargs) that returns a pandas
+    Dataframe.
+
+    The ``params[key]`` should be a :class:`pathlib.Path` to
+    save and load the cached dataframe from.
+
+    Parameters
+    ----------
+    key: str
+        key to query ``params`` to get path of cache
+
+    """
+
+    def cache_decorator(f):
+        @wraps(f)
+        def wrapper(params, force=False, **kwargs):
+            fn = params[key]
+            is_feather = fn.suffix in [".fthr"]
+            is_parq = fn.suffix in [".parq"]
+
+            if not is_feather and not is_parq:
+                raise ValueError(f"Unsupported data type: {fn}")
+
+            if fn.exists() and not force:
+                if is_feather:
+                    return pd.read_feather(fn)
+                else:
+                    return pd.read_parquet(fn)
+            output = f(params, force, **kwargs)
+            if is_feather:
+                output.to_feather(fn)
+            else:
+                output.to_parquet(fn)
+            return output
+
+        return wrapper
+
+    return cache_decorator
+
+
 def get_log_file_handler(log_fn, level=logging.INFO):
     file_handler = logging.FileHandler(log_fn)
     file_handler.setLevel(level)
-    file_handler.setFormatter(logging.Formatter('%(message)s'))
+    file_handler.setFormatter(logging.Formatter("%(message)s"))
     return file_handler
 
 
@@ -22,31 +66,35 @@ def get_stream_logger(name, level=logging.INFO):
 
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(level)
-    stream_handler.setFormatter(logging.Formatter('%(message)s'))
+    stream_handler.setFormatter(logging.Formatter("%(message)s"))
 
     logger.addHandler(stream_handler)
 
     return logger
 
 
-def get_params(root_dir=".",
-               config_fn="neptune.yaml",
-               raw_root="data/raw",
-               process_root="data/proc",
-               raw_key_root="files__raw_",
-               process_key_root="files__proc_"):
+def get_params(
+    root_dir=".",
+    config_fn="neptune.yaml",
+    raw_root="data/raw",
+    process_root="data/proc",
+    raw_key_root="files__raw_",
+    process_key_root="files__proc_",
+):
     config_fn = os.path.join(root_dir, config_fn)
     with open(config_fn, "r") as f:
         config = yaml.safe_load(f)
 
-    params = config['parameters']
+    params = config["parameters"]
     for key, value in params.items():
         if key.startswith(raw_key_root):
-            config['parameters'][key] = Path(
-                os.path.join(root_dir, raw_root, value))
+            config["parameters"][key] = Path(
+                os.path.join(root_dir, raw_root, value)
+            )
         elif key.startswith(process_key_root):
-            config['parameters'][key] = Path(
-                os.path.join(root_dir, process_root, value))
+            config["parameters"][key] = Path(
+                os.path.join(root_dir, process_root, value)
+            )
 
     return munchify(params)
 
@@ -61,13 +109,15 @@ def normalize_params(params, run_dir):
     return munchify(output)
 
 
-def run_cli(func_dict,
-            model_name,
-            tags=None,
-            root_dir=".",
-            config_fn="neptune.yaml",
-            raw_root="data/raw",
-            process_root="data/proc"):
+def run_cli(
+    func_dict,
+    model_name,
+    tags=None,
+    root_dir=".",
+    config_fn="neptune.yaml",
+    raw_root="data/raw",
+    process_root="data/proc",
+):
 
     if tags is None:
         tags = []
@@ -77,30 +127,33 @@ def run_cli(func_dict,
     func_choices = list(func_dict.keys())
 
     parser.add_argument(
-        'cmd', choices=func_choices, help='command to run on model')
-    parser.add_argument('-id', '--run-id', help='run id')
-    parser.add_argument('-d', '--debug', action='store_true', help='debug')
+        "cmd", choices=func_choices, help="command to run on model"
+    )
+    parser.add_argument("-id", "--run-id", help="run id")
+    parser.add_argument("-d", "--debug", action="store_true", help="debug")
 
     args = parser.parse_args()
     debug = args.debug
     run_id = args.run_id or datetime.datetime.utcnow().strftime(
-        '%Y-%m-%dT%H-%M-%S')
+        "%Y-%m-%dT%H-%M-%S"
+    )
 
     params = get_params(
         root_dir=root_dir,
         config_fn=config_fn,
         raw_root=raw_root,
-        process_root=process_root)
+        process_root=process_root,
+    )
 
     log_level = logging.DEBUG if debug else logging.INFO
     log = get_stream_logger(model_name, level=log_level)
 
     model_id = f"{model_name}_{run_id}"
-    run_dir = os.path.join('artifacts', model_id)
+    run_dir = os.path.join("artifacts", model_id)
 
     os.makedirs(run_dir, exist_ok=True)
 
-    log_fn = os.path.join(run_dir, f'log_{args.cmd}.txt')
+    log_fn = os.path.join(run_dir, f"log_{args.cmd}.txt")
     file_hander = get_log_file_handler(log_fn)
     log.addHandler(file_hander)
 
@@ -112,6 +165,7 @@ def run_cli(func_dict,
     neptune_ctx = None
     if "NEPTUNE_ONLINE_CONTEXT" in os.environ:
         import neptune
+
         neptune_ctx = neptune.Context()
         neptune_ctx.properties["model_id"] = model_id
         for tag in tags:
@@ -121,6 +175,7 @@ def run_cli(func_dict,
 
     if not debug:
         from mlflow import log_metric, log_artifact, log_param
+
         for k, v in output.items():
             log_metric(k, v)
             if neptune_ctx is not None:
@@ -129,24 +184,24 @@ def run_cli(func_dict,
         log_param("model_id", model_id)
 
 
-def get_classification_skorch_callbacks(model_id,
-                                        checkpoint_fn,
-                                        history_fn,
-                                        pgroups,
-                                        log_func=print,
-                                        per_epoch=True):
+def get_classification_skorch_callbacks(
+    model_id, checkpoint_fn, history_fn, pgroups, log_func=print, per_epoch=True
+):
 
     from skorch.callbacks import EpochScoring
     from skorch.callbacks import Checkpoint
 
-    from mltome.skorch.callbacks import (LRRecorder, HistorySaver,
-                                         TensorboardXLogger)
+    from mltome.skorch.callbacks import (
+        LRRecorder,
+        HistorySaver,
+        TensorboardXLogger,
+    )
 
     pgroup_names = [item[0] + "_lr" for item in pgroups]
-    tensorboard_log_dir = os.path.join('artifacts/runs', model_id)
+    tensorboard_log_dir = os.path.join("artifacts/runs", model_id)
 
-    batch_targets = ['train_loss']
-    epoch_targets = ['train_acc', 'valid_acc']
+    batch_targets = ["train_loss"]
+    epoch_targets = ["train_acc", "valid_acc"]
     if per_epoch:
         epoch_targets.extend(pgroup_names)
     else:
@@ -154,26 +209,29 @@ def get_classification_skorch_callbacks(model_id,
 
     callbacks = [
         EpochScoring(
-            'accuracy', name='train_acc', lower_is_better=False,
-            on_train=True),
+            "accuracy", name="train_acc", lower_is_better=False, on_train=True
+        ),
         LRRecorder(group_names=pgroup_names),
         TensorboardXLogger(
             tensorboard_log_dir,
             batch_targets=batch_targets,
             epoch_targets=epoch_targets,
-            epoch_groups=['acc']),
+            epoch_groups=["acc"],
+        ),
         Checkpoint(target=checkpoint_fn, sink=log_func),
-        HistorySaver(target=history_fn)
+        HistorySaver(target=history_fn),
     ]
 
     if "NEPTUNE_ONLINE_CONTEXT" in os.environ:
         from mltome.neptune import NeptuneSkorchCallback
         import neptune
+
         neptune_ctx = neptune.Context()
         neptune_callback = NeptuneSkorchCallback(
             neptune_ctx,
             batch_targets=batch_targets,
-            epoch_targets=epoch_targets)
+            epoch_targets=epoch_targets,
+        )
         callbacks.append(neptune_callback)
 
     return callbacks
